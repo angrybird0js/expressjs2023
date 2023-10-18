c9402d8f10f4d153ce55a7b46bde959e53bd1251
jaehsh <jaehwan@iMac2015.local>
2023-10-19 08:02:02 +0900

    db 지정문제 해결

--- routes/selectbook.js
+++ routes/selectbook.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var router = express.Router();
 var cors = require('cors');
 var fs = require("fs");
